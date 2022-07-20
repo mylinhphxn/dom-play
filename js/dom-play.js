e541e86345aff9d5a4d63fa244bb69e3684997ef
mylinhphxn <mylinhphxn2@outlook.com>
2022-07-20 09:48:55 -0700

    done

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,7 +1,55 @@
 //alert("hi");
 
-let actors = document.querySelectorAll(".hamlet");
+function myAlert(){
+    alert("I'm clicked!");
+}
 
-console.log(actors);
+const spans = document.querySelectorAll("#play span");
+
+console.log(spans); 
+
+for(const mySpan of spans){
+    //mySpan.addEventListener("click", myAlert);
+    
+    mySpan.addEventListener("click", function(ev){
+        //alert("The current actor is " + mySpan.dataset.actor);
+        highlightActor(mySpan.dataset.actor);
+    });
+}
 
 
+
+
+function highlightActor(actor){
+
+    for (const mySpan of spans){
+
+        if(actor == mySpan.dataset.actor){//current actor, highlight
+            mySpan.style.backgroundColor = 'yellow';
+        }else{//other actor, background white
+            mySpan.style.backgroundColor = 'white';
+        }
+
+    }
+
+
+
+    if(el.style.backgroundColor == 'yellow'){
+        el.style.backgroundColor = 'white';
+    }else{
+        el.style.backgroundColor = 'yellow';
+
+    }
+
+}
+
+function highlight(el){
+
+    if(el.style.backgroundColor == 'yellow'){
+        el.style.backgroundColor = 'white';
+    }else{
+        el.style.backgroundColor = 'yellow';
+
+    }
+
+}
